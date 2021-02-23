1949648247da8ef654f667348829f5f76cbb1597
Tarshish0906 <k.oxygen08@gmail.com>
2021-02-24 06:56:09 +0900

    テクスチャを表示させました

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,29 @@
-// 基本的なアイテムをインポート
 use nannou::prelude::*;
 
-// アプリケーションの状態を定義
-struct Model {}
+struct Model {
+    texture: wgpu::Texture,
+}
 
-// アプリケーションの開始
 fn main() {
-    // 600*400 のウィンドウを用意
-    nannou::app(model).simple_window(view).size(600, 400).run();
+    nannou::app(model).run();
 }
 
-// Modelのインスタンスを生成
-fn model(_app: &App) -> Model {
-    Model {}
+fn model(app: &App) -> Model {
+    app.new_window().size(512, 512).view(view).build().unwrap();
+    let assets = app.assets_path().unwrap();
+    let img_path = assets.join("images").join("nature").join("nature_1.jpg");
+    let texture = wgpu::Texture::from_path(app, img_path).unwrap();
+    Model { texture }
 }
 
-// 画面の描画処理
-fn view(app: &App, _model: &Model, frame: Frame) {
-    // キャンバスを取得
+fn view(app: &App, model: &Model, frame: Frame) {
+    frame.clear(BLACK);
+
+    let win = app.window_rect();
+    let r = Rect::from_w_h(100.0, 100.0).bottom_left_of(win);
+
     let draw = app.draw();
+    draw.texture(&model.texture).xy(r.xy()).wh(r.wh());
 
-    let sine = app.time.sin();
-    let slowersine1 = (app.time / 2.0).sin();
-    let slowersine2 = (app.time / 3.0).sin();
-
-    let boundary = app.window_rect();
-
-    let x = map_range(sine, -1.0, 1.0, boundary.left(), boundary.right());
-    let y1 = map_range(slowersine1, -1.0, 1.0, boundary.bottom(), boundary.top());
-    let y2 = map_range(slowersine2, -1.0, 1.0, boundary.bottom(), boundary.top());
-
-    // 背景色を設定
-    draw.background().color(NAVY);
-
-    // 半径10の円を原点に表示
-    draw.ellipse().x_y(x, y1).color(STEELBLUE);
-    draw.ellipse().x_y(x, y2).color(BLUE);
-
-    // フレームに書き出し
     draw.to_frame(app, &frame).unwrap();
 }
