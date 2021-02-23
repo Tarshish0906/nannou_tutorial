4cbd5718fbef623674ed4658dc5c64fd4f1ced42
Tarshish0906 <k.oxygen08@gmail.com>
2021-02-23 23:50:29 +0900

    正方形を実装しました

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,31 @@
-//基本的なアイテムをインポート
+// 基本的なアイテムをインポート
 use nannou::prelude::*;
 
-//アプリケーションの状態を定義
+// アプリケーションの状態を定義
 struct Model {}
 
-//アプリケーションの開始
+// アプリケーションの開始
 fn main() {
     // 600*400 のウィンドウを用意
     nannou::app(model).simple_window(view).size(600, 400).run();
 }
 
-//Modelのインスタンスを生成
+// Modelのインスタンスを生成
 fn model(_app: &App) -> Model {
     Model {}
 }
 
-//画面の描画処理
+// 画面の描画処理
 fn view(app: &App, _model: &Model, frame: Frame) {
-    //キャンバスを取得
+    // キャンバスを取得
     let draw = app.draw();
 
-    //背景色を設定
+    // 背景色を設定
     draw.background().color(NAVY);
 
     // 1辺100の正方形を原点に表示
     draw.rect().x_y(0.0, 0.0).w_h(100.0, 100.0).color(BLUE);
 
-    //フレームに書き出し
+    // フレームに書き出し
     draw.to_frame(app, &frame).unwrap();
 }
