4452f53229c56e293e7092e19e839d4abf444248
Tarshish0906 <k.oxygen08@gmail.com>
2021-02-26 21:40:27 +0900

    クリックすると色が変わるように実装しました

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,51 @@
 use nannou::prelude::*;
 
 struct Model {
-    texture: wgpu::Texture,
+    color: Rgb8,
 }
 
 fn main() {
-    nannou::app(model).run();
+    nannou::app(model).simple_window(view).size(600, 400).run();
 }
-
 fn model(app: &App) -> Model {
-    app.new_window().size(512, 512).view(view).build().unwrap();
-    let assets = app.assets_path().unwrap();
-    let img_path = assets.join("images").join("nature").join("nature_1.jpg");
-    let texture = wgpu::Texture::from_path(app, img_path).unwrap();
-    Model { texture }
+    //イベントハンドラなどを設定
+    app.new_window()
+        .size(600, 400)
+        .mouse_pressed(mouse_pressed)
+        .view(view)
+        .build()
+        .unwrap();
+
+    //色の初期値を指定してインスタンス生成
+    Model {
+        color: gen_random_color(),
+    }
 }
 
+//クリック時のイベントハンドラ
+fn mouse_pressed(_app: &App, model: &mut Model, _button: MouseButton) {
+    model.color = gen_random_color()
+}
+
+// ランダムな色を返す自作関数
+fn gen_random_color() -> Rgb8 {
+    let r = random::<u8>();
+    let g = random::<u8>();
+    let b = random::<u8>();
+    let random_color = rgb8(r, g, b);
+    random_color
+}
+//アプリケーションが起動している間、ループ
 fn view(app: &App, model: &Model, frame: Frame) {
-    frame.clear(BLACK);
-
-    let win = app.window_rect();
-    let r = Rect::from_w_h(200.0, 100.0).bottom_left_of(win);
-
     let draw = app.draw();
-    draw.texture(&model.texture).xy(r.xy()).wh(r.wh());
+
+    draw.background().color(WHITE);
+
+    draw.rect()
+        //設定した色で塗りつぶす
+        .color(model.color)
+        .x_y(0.0, 0.0)
+        .w_h(100.0, 100.0);
 
     draw.to_frame(app, &frame).unwrap();
 }
