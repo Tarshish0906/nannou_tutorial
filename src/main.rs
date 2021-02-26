19d06a4e943059ea3482b080cfc984bb7c25ed3d
Tarshish0906 <k.oxygen08@gmail.com>
2021-02-26 22:09:28 +0900

    wgpuを使いました

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,70 @@
+use nannou::image;
+use nannou::noise::{Fbm, MultiFractal, NoiseFn};
 use nannou::prelude::*;
 
 struct Model {
-    color: Rgb8,
+    texture: wgpu::Texture,
+    noise: Fbm,
 }
 
 fn main() {
-    nannou::app(model).simple_window(view).size(600, 400).run();
+    nannou::app(model).run();
 }
+
 fn model(app: &App) -> Model {
     //イベントハンドラなどを設定
-    app.new_window()
-        .size(600, 400)
-        .mouse_pressed(mouse_pressed)
-        .view(view)
-        .build()
-        .unwrap();
+    app.new_window().size(600, 1200).view(view).build().unwrap();
 
-    //色の初期値を指定してインスタンス生成
+    let window = app.main_window();
+    let win = window.rect();
+    let texture = wgpu::TextureBuilder::new()
+        .size([win.w() as u32, win.h() as u32])
+        .format(wgpu::TextureFormat::Rgba8Unorm)
+        .usage(wgpu::TextureUsage::COPY_DST | wgpu::TextureUsage::SAMPLED)
+        .build(window.swap_chain_device());
+
     Model {
-        color: gen_random_color(),
+        texture,
+        noise: Fbm::new().set_octaves(5).set_persistence(0.5 as f64),
     }
 }
 
-//クリック時のイベントハンドラ
-fn mouse_pressed(_app: &App, model: &mut Model, _button: MouseButton) {
-    model.color = gen_random_color()
-}
-
-// ランダムな色を返す自作関数
-fn gen_random_color() -> Rgb8 {
-    let r = random::<u8>();
-    let g = random::<u8>();
-    let b = random::<u8>();
-    let random_color = rgb8(r, g, b);
-    random_color
-}
 //アプリケーションが起動している間、ループ
 fn view(app: &App, model: &Model, frame: Frame) {
+    frame.clear(BLACK);
+
+    let win = app.window_rect();
+    let noise = &model.noise;
+
+    let noise_x_range = win.w() / 50.0;
+    let noise_y_range = win.h() / 50.0;
+
+    let image = image::ImageBuffer::from_fn(win.w() as u32, win.h() as u32, |x, y| {
+        let noise_x = map_range(x, 0, win.w() as u32, 0.0, noise_x_range) as f64;
+        let noise_y = map_range(y, 0, win.h() as u32, 0.0, noise_y_range) as f64;
+        let noise_value = map_range(
+            noise.get([noise_x, noise_y, app.time as f64]),
+            1.0,
+            -1.0,
+            0.0,
+            std::u8::MAX as f64,
+        );
+        let n = noise_value as u8;
+        if x % 10 == 5 && y % 10 == 5 {
+            return nannou::image::Rgba([n, n, 0, std::u8::MAX]);
+        }
+        nannou::image::Rgba([0, 0, 0, std::u8::MAX])
+    });
+
+    let flat_samples = image.as_flat_samples();
+    model.texture.upload_data(
+        app.main_window().swap_chain_device(),
+        &mut *frame.command_encoder(),
+        &flat_samples.as_slice(),
+    );
+
     let draw = app.draw();
-
-    draw.background().color(WHITE);
-
-    draw.rect()
-        //設定した色で塗りつぶす
-        .color(model.color)
-        .x_y(0.0, 0.0)
-        .w_h(100.0, 100.0);
+    draw.texture(&model.texture);
 
     draw.to_frame(app, &frame).unwrap();
 }
